a33d17fbb6db8227ecedbb2376bfaa103e136ffb
Renz Anthony Rackhold <rackhold@gmail.com>
2025-06-07 16:21:18 +0800

    add a class-based views with use of mixins

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins, generics
 
 from students.models import Student
 from .serializers import StudentSerializer
@@ -57,48 +58,94 @@ def studentDetailView(request, student_id):
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-class EmployeesView(APIView):
-    # GET method to retrieve all employees
-    def get(self, request):
-        employees = Employee.objects.all()
-        serializer = EmployeeSerializer(employees, many=True) #Many=True indicates that the serializer should expect a list of objects
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class EmployeesView(APIView):
+#     # GET method to retrieve all employees
+#     def get(self, request):
+#         employees = Employee.objects.all()
+#         serializer = EmployeeSerializer(employees, many=True) #Many=True indicates that the serializer should expect a list of objects
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     # POST method to create a new employee
+#     def post(self, request):
+#         serializer = EmployeeSerializer(data=request.data) # data=request.data indicates that the serializer should use the data from the request
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# class EmployeeDetailView(APIView):
+#     # Helper method to get an employee object by ID
+#     def get_object(self, employee_id):
+#         try:
+#             return Employee.objects.get(pk=employee_id)
+#         except Employee.DoesNotExist:
+#             raise Http404
+#
+#     # GET method to retrieve a specific employee by ID
+#     def get(self, request, employee_id):
+#         employee = self.get_object(employee_id) # Use the helper method to get the employee object
+#         serializer = EmployeeSerializer(employee)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     # PUT method to update a specific employee by ID
+#     def put(self, request, employee_id):
+#         employee = self.get_object(employee_id)
+#
+#         serializer = EmployeeSerializer(employee, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     # DELETE method to delete a specific employee by ID
+#     def delete(self, request, employee_id):
+#        employee = self.get_object(employee_id)
+#
+#        employee.delete()
+#        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    # POST method to create a new employee
-    def post(self, request):
-        serializer = EmployeeSerializer(data=request.data) # data=request.data indicates that the serializer should use the data from the request
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class EmployeeDetailView(APIView):
-    # Helper method to get an employee object by ID
-    def get_object(self, employee_id):
-        try:
-            return Employee.objects.get(pk=employee_id)
-        except Employee.DoesNotExist:
-            raise Http404
 
-    # GET method to retrieve a specific employee by ID
-    def get(self, request, employee_id):
-        employee = self.get_object(employee_id) # Use the helper method to get the employee object
-        serializer = EmployeeSerializer(employee)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class EmployeesView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+        """
+        A view for handling Employee objects using Django REST Framework's mixins and generics.
 
-    # PUT method to update a specific employee by ID
-    def put(self, request, employee_id):
-        employee = self.get_object(employee_id)
+        This view supports:
+        - Listing all Employee objects (GET method)
+        - Creating a new Employee object (POST method)
 
-        serializer = EmployeeSerializer(employee, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        Attributes:
+            queryset: The queryset of Employee objects to be used by the view.
+            serializer_class: The serializer class to be used for serializing and deserializing Employee objects.
+        """
+        queryset = Employee.objects.all()
+        serializer_class = EmployeeSerializer
 
-    # DELETE method to delete a specific employee by ID
-    def delete(self, request, employee_id):
-       employee = self.get_object(employee_id)
+        def get(self, request):
+            """
+            Handles GET requests to list all Employee objects.
 
-       employee.delete()
-       return Response(status=status.HTTP_204_NO_CONTENT)
+            Args:
+                request: The HTTP request object.
+
+            Returns:
+                Response: A Response object containing serialized Employee data and an HTTP 200 status.
+            """
+            return self.list(request)
+
+        def post(self, request):
+            """
+            Handles POST requests to create a new Employee object.
+
+            Args:
+                request: The HTTP request object containing the data for the new Employee.
+
+            Returns:
+                Response: A Response object containing the serialized data of the created Employee and an HTTP 201 status,
+                          or errors and an HTTP 400 status if the data is invalid.
+            """
+            return self.create(request)
+
+
+class EmployeeDetailView(generics.GenericAPIView):
+    pass
